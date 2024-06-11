a4a06e9aae125c13ce521eecbb1b4aaa4a31bf60
Blast1FM <kingprostyle@yandex.ru>
2024-06-11 22:32:14 +0300

    added cohesion this logic is bad and inefficient

--- src/boid.c
+++ src/boid.c
@@ -50,30 +50,48 @@ Vector2 getAverageNeighbourVelocity(Boid* boid, int flockArrayLength)
             alignmentY += boid->flock[i].velocity.y;
         }
     }
-
+    if(neighbours == 0) return Vector2Zero();
     return (Vector2){alignmentX/(float)neighbours, alignmentY/(float)neighbours};
 }
 
 Vector2 getCohesionVelocity(Boid* boid, int flockArrayLength)
 {
-    return Vector2Zero();
+    float averageX = 0;
+    float averageY = 0;
+    float neighbours = 0;
+
+    for(int i = 0; i<flockArrayLength; i++)
+    {
+        if(boid==&boid->flock[i]) continue;
+        if(Vector2Distance(boid->position, boid->flock[i].position)<BOID_VISIBILITY_RADIUS)
+        {
+            neighbours+=1;
+            averageX += boid->flock[i].position.x;
+            averageY += boid->flock[i].position.y;
+        }
+    }
+    if (neighbours == 0) return Vector2Zero();
+    return (Vector2){averageX-boid->position.x, averageY-boid->position.y};
 }
 
 void updateBoid(Boid* boid, int flockArrayLength)
 {
     double deltaTime = GetFrameTime();
     float avoidFactor = 0.5f;
-    float alignmentFactor = 1.0f;
+    float alignmentFactor = 0.5f;
+    float cohesionFactor = 1.0f;
     Vector2 resultingVelocity = boid->velocity;
     
     Vector2 separationVelocity = getSeparationVelocity(boid, flockArrayLength);
     Vector2 alignmentVelocity = getAverageNeighbourVelocity(boid, flockArrayLength);
-
+    Vector2 cohesionVelocity = getCohesionVelocity(boid, flockArrayLength);
 
     Vector2 resultingVelocitySeparation = Vector2Add(resultingVelocity, Vector2Scale(separationVelocity, avoidFactor));
-    Vector2 resultingVelocityAlignment = Vector2Scale(Vector2Subtract(alignmentVelocity, (Vector2){boid->velocity.x,boid->velocity.y}),alignmentFactor);
+    Vector2 resultingVelocityAlignment = Vector2Scale(Vector2Subtract(alignmentVelocity, boid->velocity),alignmentFactor);
+    Vector2 resultingVelocityCohesion = Vector2Scale(cohesionVelocity, cohesionFactor);
 
     resultingVelocity = Vector2Add(resultingVelocitySeparation, resultingVelocityAlignment);
+    resultingVelocity = Vector2Add(resultingVelocity, resultingVelocityCohesion);
 
     boid->velocity = Vector2Add(boid->velocity, resultingVelocity);
     boid->position.x = boid->position.x + boid->velocity.x * deltaTime;
